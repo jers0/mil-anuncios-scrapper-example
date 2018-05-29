162818c8c5fd26bdf3757dc404b6ed73948a1844
Izhar <izhar.sanz@gmail.com>
2018-05-29 17:22:46 +0200

    Commit inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+const Nightmare = require('nightmare');
+
+(async function init(){
+
+    const nightmare = Nightmare({ maxHeight:2500, minWidth:1600, show: true, webPreferences: {partition: 'mil-anuncios'}});
+    await nightmare.goto('https://www.milanuncios.com/anuncios-en-valencia/coche.htm');
+    await nightmare.useragent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    await nightmare.wait(2000);
+
+    (async function loop(count = 0){
+        console.log('count', count);
+        await nightmare.goto('http://www.google.es');
+        await nightmare.goto('https://www.milanuncios.com/anuncios-en-valencia/coche.htm');
+        await nightmare.wait(2000);
+            
+        if(count > 20){
+            await nightmare.end();
+            return true;
+        }else{
+            loop(count+1)
+        }
+
+    })();
+})();
